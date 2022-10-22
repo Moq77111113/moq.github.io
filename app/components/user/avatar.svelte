d12c7e2531bfb3b2c9f7a5da0877b687b1aad1cf
Quentin Moessner <q.moessner@yamay.app>
2022-10-22 13:50:25 +0200

    feat(v1.0.0): add basic content

--- app/components/user/avatar.svelte
+++ app/components/user/avatar.svelte
@@ -1,10 +1,7 @@
 <svelte:options tag="ui-avatar" />
 
 <script lang="ts">
-  import type { User } from "../../types/user";
-
   export let src: string;
-  export let name: string;
   export let email: string;
 </script>
 
@@ -16,19 +13,14 @@
     style="max-width: 100%; border-radius: 9999px;"
     on:error={() => (src = "/img/ui/avatar.svg")}
   />
-  <span class="label">{name}</span>
 </a>
 
 <style lang="scss">
   img {
-    @apply w-32 h-32;
+    @apply w-40 h-40;
   }
   .wrap {
     @apply relative top-1 overflow-clip;
-  }
-
-  .label {
-    @apply relative left-1/2 -translate-x-1/2 font-display text-center block mx-auto px-2 text-lg rounded-sm shadow-lg cursor-pointer;
   }
 
   a {
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import "../styles/app.scss";
 
 export * from "./components/demo/hi-you.svelte";
-export * from "./components/user/avatar.svelte"
+export * from "./components/user/avatar.svelte";
+export * from "./components/ui/scroll-up.svelte";
+export * from "./components/ui/scroll-show.svelte";
