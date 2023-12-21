2a4865c7479645884ba47bd117bff74637d2c945
Quentin <quentin.moessner@bellman.immo>
2023-12-21 17:47:12 +0100

    feat(clean): old website

--- app/main.ts
+++
@@ -1,9 +0,0 @@
-import "../styles/app.scss";
-
-export * from "./components/demo/hi-you.svelte";
-export * from "./components/ui/avatar.svelte";
-export * from "./components/ui/scroll-up.svelte";
-export * from "./components/ui/scroll-show.svelte";
-export * from "./components/ui/tag-list.svelte";
-export * from "./components/ui/switch-theme.svelte";
-export * from "./components/ui/skill-bar.svelte";
--- content/_index.md
+++
@@ -1,6 +0,0 @@
----
-title: Quentin Moessner
-lastmod: 2022-10-30T009:00:00+02:00
-draft: false
-description:
----
