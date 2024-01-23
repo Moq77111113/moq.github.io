a9a8ecf4333fca5b611f654ee2195f977c548f4a
Quentin <quentin.moessner@bellman.immo>
2024-01-23 08:34:36 +0100

    style: small nav & details on about section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,41 @@
 <script lang="ts">
   import Company from '$lib/components/Company.svelte'
   import List from '$lib/components/List.svelte'
-  import Selection from '$lib/components/Selection.svelte'
+  import About from '$lib/components/About.svelte'
+  import { Play } from 'lucide-svelte'
   import Skill from '$lib/components/Skill.svelte'
   import { Button } from '$lib/components/ui/button'
   import { companies } from '$lib/data/companies'
   import { skills } from '$lib/data/skills'
 </script>
 
-<main class="container">
+<main class="container max-w-screen-lg">
+  <!-- Nav -->
+  <nav class="my-6 flex w-full flex-row">
+    <Button
+      size="icon"
+      class="flex h-12 w-12 rounded-full bg-green-spotify hover:scale-110 hover:bg-green-spotify/90"
+      ><Play color="#000" fill="#000" size={20} /></Button
+    >
+  </nav>
+
   <!-- Skills -->
   <section
-    class="flex flex-col justify-between gap-6 md:grid-cols-2 md:flex-row"
+    class="mt-2 flex flex-col justify-between gap-6 md:mt-0 md:grid-cols-2 md:flex-row"
   >
     <List
       title={'Skills'}
       items={skills}
-      class={['md:min-w-[400px]']}
+      class={['order-2 md:order-1 md:min-w-[400px]']}
       showItems={5}
     >
+      <div slot="title" class="mt-2 flex flex-col items-start">
+        <h2 class="text-2xl font-semibold">Skills</h2>
+        <span
+          class=" bg-gradient-to-r from-blue-400 to-red-600 bg-clip-text text-xs lowercase text-transparent"
+          ><small class="align-middle">❤</small> stack</span
+        >
+      </div>
       <svelte:fragment slot="item" let:item let:index>
         <div
           class="flex grow-0 items-center rounded px-1 py-2 hover:bg-white/5"
@@ -32,12 +49,13 @@
       </svelte:fragment>
     </List>
 
-    <div class="mb-3 flex flex-grow flex-col items-start gap-2">
-      <div class="mt-2 flex items-start justify-between">
-        <h2 class="text-2xl font-semibold">Actualité</h2>
+    <!-- About -->
+    <div class="order-1 flex flex-grow flex-col items-start gap-2 md:order-2">
+      <div class="mb-3 mt-2 flex items-start justify-between">
+        <h2 class="text-2xl font-semibold">About</h2>
       </div>
 
-      <Selection />
+      <About />
     </div>
   </section>
 
